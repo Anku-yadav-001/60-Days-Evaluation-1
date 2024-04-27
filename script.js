166ff68342fb36b69007d280c366ec935bfffa46
Anku-yadav-001 <yaman49751@gmail.com>
2024-04-27 22:36:44 +0530

    done

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ async function fetchData(category){
     container.innerHTML=""
     let URL=`https://fakestoreapi.com/products`
     if(category=="electronics" || category=="jewelery" || category=="men's clothing" || category=="women's clothing"){
-        URL=URL+`/category/${category}`
+        URL=URL+`/category/${category}` //filter on  the basis of catagory
     }
   if(category=="asc" || category=="desc"){
-    URL=URL+`?sort=${category}`
+    URL=URL+`?sort=${category}` //filter on the basis of price
   }
 
     try {
         let response=await fetch(URL)
         let answer=await response.json();
-        displayData(answer);
+        displayData(answer);//invoke the displayData function with the data coming from the API
     } catch (error) {
         console.log(error);
     }
 }
 
-fetchData()
+fetchData() //initally, invoke the function for show the products on UI
 function displayData(products){
       products.map((ele)=>{
         let div=document.createElement("div")
@@ -35,36 +35,38 @@ function displayData(products){
         title.innerHTML=`<strong>Title:</strong> ${ele.title}`;
         price.innerHTML=`<strong>Price:</strong> $${ele.price}`
 
-        div.append(image,title,price)
-        container.appendChild(div)
+        div.append(image,title,price)//append the image,title and price in div
+        container.appendChild(div)//and append div in container
       })
 }
 product_catagories.addEventListener("change",function(){
-          fetchData(product_catagories.value)//catagories
+          fetchData(product_catagories.value)//take catagory from UI
 })
 sort_price.addEventListener("change",function(){
-    fetchData(sort_price.value)//price
+    fetchData(sort_price.value)//take the value of price dorting form UI
 })
 search_box.addEventListener("input",function(){
-     fetchData(search_box.value)//search
+     fetchData(search_box.value)//input with the help of Input and take the search value
 })
-// let timer;
-// async function handleSearch(title){
-//     let resp=await fetch("https://fakestoreapi.com/products")
-//     let ans=await resp.json();
+
+// search functionality
+let timer;
+async function handleSearch(title){
+    let resp=await fetch("https://fakestoreapi.com/products")
+    let ans=await resp.json();
      
-//     return ans.filter(items=>items.title.toLowerCase().includes(title.toLowerCase()))
+    return ans.filter(items=>items.title.toLowerCase().includes(title.toLowerCase()))
    
-// }
-//  function handleInput(title){
-//         clearTimeout(timer)
-//         timer=setTimeout(()=>{
-//             let result=handleSearch(title)
-//             console.log(result);
-//             // result.map(element => {
-//             //     container.innerHTML=""
-//             //     container.textContent=element.title;
-//             //     console.log(container);
-//             // });
-//         },3000)
-// }
+}
+ function handleInput(title){
+        clearTimeout(timer)
+        timer=setTimeout(()=>{
+            let result=handleSearch(title)
+            console.log(result);
+            result.map(element => {
+                container.innerHTML=""
+                container.textContent=element.title;
+                console.log(container);
+            });
+        },3000)
+}
